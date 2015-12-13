f9eae9b81c3fbb774e8ebef96d864520ad1773c6
Erik Humphrey <ContactErikH@gmail.com>
2015-12-13 18:05:04 -0500

    Initial commit

---
+++ HumphreyErik21Shapes/Form1.cs
@@ -0,0 +1,27 @@
+﻿/* Author: Erik Humphrey
+ * Project title: Project 21 - Shapes
+ * Date started: December 13th 2015
+ * Executable name: HumphreyErik20Shapes.exe
+ * Description: Program that repetitively draws random coloured ellipses, rectangles, and lines
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumphreyErik21Shapes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
